c73451bd78faeb491844a1dd876ce41843bd8585
Carlos OH Rivera <cariveh@gmail.com>
2023-04-11 15:20:58 -0600

    eureka

--- moto-service/src/main/java/com/oh/motoservice/motoServiceApplication.java
+++ moto-service/src/main/java/com/oh/motoservice/motoServiceApplication.java
@@ -2,8 +2,10 @@ package com.oh.motoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class motoServiceApplication {
 
 	public static void main(String[] args) {
--- usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -2,13 +2,16 @@ package com.usuario.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 // Aquí me he quedado -- que no guarda el id del cliente en el POST http://localhost:8002/usuario/carro/2
 // https://youtu.be/icTg6iTqpUk?t=5915
 @EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
+
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
